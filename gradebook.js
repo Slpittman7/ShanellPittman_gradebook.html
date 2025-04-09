daf29b0b75338f64cc85f9a92b1fec88f6305f8e
Slpittman7 <slpitman@my.waketech.edu>
2025-04-09 19:19:10 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,17 @@
+console.log("Shanell Pittman's Gradebook JavaScript loaded.");
 
+// Stub function for fetching data
+function fetchGradeData() {
+  console.log("fetchGradeData() called.");
+  // Future database fetch will go here
+}
+
+// Stub function for populating gradebook
+function populateGradebook() {
+  console.log("populateGradebook() called.");
+  // Will be used to populate table dynamically
+}
+
+// Initial calls to check stubs
+fetchGradeData();
+populateGradebook();
